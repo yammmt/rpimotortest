29ed447bb36cf2da8e5af9506b21d40803e0c367
yammmt <cu.cs.y20@gmail.com>
2021-09-23 20:28:39 +0900

    Allow continuous execution

--- degree_arg.py
+++ degree_arg.py
@@ -12,10 +12,14 @@ time.sleep(0.5)
 print("degree: ", end="")
 move_deg = float(input())
 
-full_step_num = move_deg / FULL_DEG_ONE_STEP // 4
-print("full step x ", full_step_num)
+while move_deg >= 0.0:
+    full_step_num = move_deg / FULL_DEG_ONE_STEP // 4
+    print("full step x ", full_step_num)
 
-mymotor.motor_run(GPIO_PINS, 0.1, full_step_num, False, False, "full", .05)
+    mymotor.motor_run(GPIO_PINS, 0.1, full_step_num, False, False, "full", .05)
+
+    print("degree: ", end="")
+    move_deg = float(input())
 
 # good practise to cleanup GPIO at some point before exit
 GPIO.cleanup()
