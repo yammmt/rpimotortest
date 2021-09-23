2aaae1ff7c9570fb2113bec927d97855044ad9fa
yammmt <cu.cs.y20@gmail.com>
2021-09-23 20:18:52 +0900

    Allow user input

--- degree_arg.py
+++ degree_arg.py
@@ -4,13 +4,15 @@ import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 
 GPIO_PINS = [17, 18, 27, 22]
-MOVE_DEG = 90.0
 FULL_DEG_ONE_STEP = 1.8
 
 mymotor = RpiMotorLib.BYJMotor("mymotor", "Nema")
 time.sleep(0.5)
 
-full_step_num = MOVE_DEG / FULL_DEG_ONE_STEP // 4
+print("degree: ", end="")
+move_deg = float(input())
+
+full_step_num = move_deg / FULL_DEG_ONE_STEP // 4
 print("full step x ", full_step_num)
 
 mymotor.motor_run(GPIO_PINS, 0.1, full_step_num, False, False, "full", .05)
